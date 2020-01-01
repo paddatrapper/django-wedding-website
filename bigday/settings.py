f83e3cf35e3cfb9f20716569ed1a97c6ace13ef9
Kyle Robbertze <kyle@paddatrapper.com>
2020-01-01 16:24:32 +0200

    add settings

--- bigday/settings.py
+++ bigday/settings.py
@@ -127,18 +127,18 @@ STATICFILES_DIRS = (
 )
 
 # the address your emails (save the dates/invites/etc.) will come from
-DEFAULT_WEDDING_FROM_EMAIL = 'You and Your Partner <happilyeverafter@example.com>'
+DEFAULT_WEDDING_FROM_EMAIL = 'Kyle and Jessica <us@kyleandjess.co.za>'
 # the default reply-to of your emails
-DEFAULT_WEDDING_REPLY_EMAIL = 'happilyeverafter@example.com'
+DEFAULT_WEDDING_REPLY_EMAIL = 'rsvp@kyleandjess.co.za'
 
 # when sending test emails it will use this address
 DEFAULT_WEDDING_TEST_EMAIL = DEFAULT_WEDDING_FROM_EMAIL
 
 # This is used in a few places where the names of the couple are used
-BRIDE_AND_GROOM = 'Cory and Rowena'
+BRIDE_AND_GROOM = 'Kyle and Jessica'
 
 # This is used in links in save the date / invitations
-WEDDING_WEBSITE_URL = 'http://thehappycouple.com'
+WEDDING_WEBSITE_URL = 'https://kyleandjess.co.za'
 WEDDING_CC_LIST = []  # put email addresses here if you want to cc someone on all your invitations
 
 # change to a real email backend in production
